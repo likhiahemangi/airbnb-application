66401e1cf4a805a5597c696314fdffbb0f9111e5
Hemangi Likhia <n01380076@nj-00118-013.humber.org>
2020-02-20 16:42:32 -0500

    dashboard

--- models/roomlisting.js
+++ models/roomlisting.js
@@ -6,15 +6,15 @@ const prouduct =
     init()
     {
 
-        this.fakeDB.push({title:'Seahell house [Etobicoke]',price:`$393 CAD/Night`,rating:'4.6<span class="fa fa-star checked"></span>'});
-        this.fakeDB.push({title:'Romatic Cabine House [DownTown]',price:`$363 CAD/Night`,rating:'4.3<span class="fa fa-star checked"></span>'});
-        this.fakeDB.push({title:'New York house [DownTown]',price:`$300 CAD/Night`,rating:'4.5<span class="fa fa-star checked"></span>'});
-        this.fakeDB.push({title:'Seahell house [Markham]',price:`$385 CAD/Night`,rating:'4.0<span class="fa fa-star checked"></span>'});
-        this.fakeDB.push({title:'Tree house [Etobicoke]',price:`$250 CAD/Night`,rating:'4.3<span class="fa fa-star checked"></span>'});
-        this.fakeDB.push({title:'Cottage house [Habourfront Side]',price:`$295 CAD/Night`,rating:'4.6<span class="fa fa-star checked"></span>'});
-        this.fakeDB.push({title:'Mashroom Doom house [vaughan]',price:`$390 CAD/Night`,rating:'4.2<span class="fa fa-star checked"></span>'});
-        this.fakeDB.push({title:'Seahell house [DownTown]',price:`$395 CAD/Night`,rating:'4.5<span class="fa fa-star checked"></span>'});
-        this.fakeDB.push({title:'Down hall house [Etobicoke]',price:`$215 CAD/Night`,rating:'4.8<span class="fa fa-star checked"></span>'});
+        this.fakeDB.push({image:'/images/T1.jpeg',title:'Seahell house [Etobicoke]',price:`$393 CAD/Night`});
+        this.fakeDB.push({image:'/images/T2.jpeg',title:'Romatic Cabine House [DownTown]',price:`$363 CAD/Night`});
+        this.fakeDB.push({image:'/images/T3.jpeg',title:'New York house [DownTown]',price:`$300 CAD/Night`});
+        this.fakeDB.push({image:'/images/T4.jpg',title:'Seahell house [Markham]',price:`$385 CAD/Night`});
+        this.fakeDB.push({image:'/images/T5.jpg',title:'Tree house [Etobicoke]',price:`$250 CAD/Night`});
+        this.fakeDB.push({image:'/images/T6.jpeg',title:'Cottage house [Habourfront Side]',price:`$295 CAD/Night`});
+        this.fakeDB.push({image:'/images/T7.jpeg',title:'Mashroom Doom house [vaughan]',price:`$390 CAD/Night`});
+        this.fakeDB.push({image:'/images/T8.jpeg',title:'Seahell house [DownTown]',price:`$395 CAD/Night`});
+        this.fakeDB.push({image:'/images/T2.jpeg',title:'Down hall house [Etobicoke]',price:`$215 CAD/Night`});
     
     },
     getallProducts()
